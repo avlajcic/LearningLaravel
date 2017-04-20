51fccefef7eaf7c1e0e14a059d9ad462584f8874
avlajcic <armando.vlajcic@gmail.com>
2017-04-20 18:26:01 +0200

    (Another) possible fix for CORS and closed debugbar

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://laravel-learning2017.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
